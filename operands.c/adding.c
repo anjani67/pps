ba1fba218a1379aec7e04be449d278c96ae236e7
anjani67 <120377591+anjani67@users.noreply.github.com>
2023-01-29 22:52:21 +0530

    Add files via upload

---
+++ operands.c/adding.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int first_number;
+printf("enter first_number");
+scanf("%d",&first_number);
+printf("first number after adding %d",first_number+50);
+printf("first number before %d after %d",first_number,first_number+50);
+return;
+}
+
+
